d747476eff55772e0d8a4b5441f98c2e439e2f02
Manoranjan <manoranjanhandique@gmail.com>
2025-02-04 00:04:52 +0530

    step 4- passed test cases for support different delimiters

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -3,9 +3,17 @@ const add = (numbers) => {
     return 0;
   }
 
-  return numbers
-    .replace(/\n/g, ",")
-    .split(",")
+  let delimiter = /[\n,]/;
+
+  if (numbers.slice(0, 2) === "//") {  
+    const parts = numbers.split("\n");
+    delimiter = parts[0].substring(2); 
+    numbers = parts[1]; 
+  }
+
+  const numArray = numbers.split(delimiter);
+
+  return numArray
     .map(Number)
     .reduce((sum, num) => sum + num, 0);
 };
