9cf6eb69afd13854fa6038b97caf74c527648e44
Manoranjan <manoranjanhandique@gmail.com>
2025-02-03 22:14:48 +0530

    2nd test case failed for 1

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -3,3 +3,7 @@ const {add}=require('../src/stringCalculator')
 test('returns 0 for an empty string', () => {
     expect(add("")).toBe(0);
 });
+
+test("returns 1 when input is '1'", () => {
+    expect(add("1")).toBe(1);
+  });
