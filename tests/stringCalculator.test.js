357c8806c823e1784cbae36d757ba09f12065c10
Manoranjan <manoranjanhandique@gmail.com>
2025-02-03 22:19:30 +0530

    third failed test case for string "1" and "5"

--- tests/stringCalculator.test.js
+++ tests/stringCalculator.test.js
@@ -7,3 +7,7 @@ test('returns 0 for an empty string', () => {
 test("returns 1 when input is '1'", () => {
     expect(add("1")).toBe(1);
   });
+
+  test("returns sum of two numbers separated by a comma", () => {
+    expect(add("1,5")).toBe(6);
+  });
